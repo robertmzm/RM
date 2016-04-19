af530a30878194e6e7e94b21dfa46ef859585c67
RobertMo <robertmozhaoming@gmail.com>
2016-04-19 01:47:23 -0700

    edited indention

--- RM.c
+++ RM.c
@@ -27,7 +27,7 @@
         *start to use github in order to work on different device;
         *trying to make some changes from git;
         *making changes from mac!!!!!!!;
-	*making changes from windows!!!!;
+		*making changes from windows!!!!;
 	
 */
 #define STOP 360
